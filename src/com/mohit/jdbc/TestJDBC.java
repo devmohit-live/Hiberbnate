9bce2153532bb0b5e62060d57517928f1b4debac
Mohit <mohitsingh.it5@gmail.com>
2022-05-14 15:36:35 +0530

    JDBC connection done | Mohit

---
+++ src/com/mohit/jdbc/TestJDBC.java
@@ -0,0 +1,23 @@
+package com.mohit.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBC {
+
+	public static void main(String[] args) {
+		final String jdbcURL = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		final String user = "hbstudent";
+		final String pass = "mohit";
+				
+		try {
+			System.out.println("Connecting yo db "+jdbcURL);
+			Connection myconn = DriverManager.getConnection(jdbcURL,user,pass);
+			System.out.println("Connection Succesfull!");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
