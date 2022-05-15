908675d8701d1b92c43c8e2304a854e971fcb5a8
Mohit <mohitsingh.it5@gmail.com>
2022-05-15 19:11:26 +0530

    Updating multiple students using condition in DB | Mohit

--- src/com/mohit/demo/UpdateStudentDemo.java
+++ src/com/mohit/demo/UpdateStudentDemo.java
@@ -26,7 +26,16 @@ public class UpdateStudentDemo {
 			System.out.println(session.get(Student.class, 1));
 			session.getTransaction().commit();
 			
-			//remember after you commit you hae to again begin a new transaction to perform any operation
+			//remember after you commit you have to again get a new session and begin a new transaction to perform any operation
+			
+			//updating multiple objects based of condition 
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			session.createQuery("update Student set email='foobar@io.com' where email LIKE '%bar.com' ").executeUpdate();
+			
+			
+			session.getTransaction().commit();
 			
 			
 		}finally {
