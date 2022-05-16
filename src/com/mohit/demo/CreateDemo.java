e7b4df62ccfa059a6edf20ebfa50aefffa701ed3
Mohit <mohitsingh.it5@gmail.com>
2022-05-16 16:27:21 +0530

    Created One to One Unidirectional Mapping with Cascading | Mohit

---
+++ src/com/mohit/demo/CreateDemo.java
@@ -0,0 +1,35 @@
+package com.mohit.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mohit.hibernate.entity.Instructor;
+import com.mohit.hibernate.entity.InstructorDetail;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		try {
+			//start transaction
+			session.beginTransaction();
+			
+			//create Instructor
+			Instructor instructor = new Instructor("Mike","Tyson","tyson@mike.com");
+			InstructorDetail details = new InstructorDetail("www.youtube.com/realmiketyson","Feeding to pet Pigeons");
+			instructor.setInstructor_detail(details);
+			session.save(instructor); //cascading all will save the details too
+//			one to one unidirectional cascade => instructor->instructor_details
+			session.getTransaction().commit();
+		}finally {
+			factory.close();
+		}
+
+	}
+
+}
